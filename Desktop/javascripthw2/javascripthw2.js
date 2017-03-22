f979a10634abebb6153a08c2d5d38d21935cacae
jeanette montesano <escapetheearth@gmail.com>
2017-03-22 16:22:28 -0400

    javascripthw2

---
+++ Desktop/javascripthw2/javascripthw2.js
@@ -0,0 +1,54 @@
+alert("hello crystal")
+
+var num1 = 5
+var num2 = 2
+console.log(num1 + num2)
+
+var string1 = "hello"
+var string2 = "kitty"
+console.log(string1 + string2)
+
+var listOfNames = ["pink", "purple", "blue"]
+console.log(listOfNames[0], listOfNames[1])
+
+var listOfNew = ["red", "blue", "green"]
+var listOfAlso = [listOfNew, listOfNames]
+console.log(listOfAlso[0][2])
+
+var var1 = prompt("enter a number")
+if (Var1 < 100){
+alert("Var1 is less than 100")
+} else { 
+alert("Var1 is less than 100")
+}
+
+var var2 = "this is a string"
+var var3 = "this is also a string"
+if (var2 == var3) {
+	console.log ("these strings are equal")
+} else {
+	console.log("these strings are different")
+}
+
+var opulentTemple = prompt ("please enter a name")
+
+function newName ()
+{
+alert ("your name is" + opulentTemple)
+}
+newName (opulentTemple)
+
+function noArgs () {
+	console.log("hi")
+}
+
+function doorPrize(doorNumber) {
+	if(doorNumber == 1) {
+	alert("your prize is a crystal")
+	}else if(doorNumber ==2){
+		alert("your prize is a gem")
+	}else if(doorNumber ==3){
+		alert("your prize is coins")
+	}
+}
+
